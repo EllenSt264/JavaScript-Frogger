19174a1d67e148da65730e3ce4a0a6f13b9483ad
EllenSt264 <stafford.ellen@protonmail.com>
2020-10-26 20:33:14 +0000

    Add feat: keydown/keyup event listeners so the player can move frog

--- assets/js/frogger.js
+++ assets/js/frogger.js
@@ -11,11 +11,37 @@ class Frogger {
         this.frameY = 0;
     }
     update() {
-        console.log("update");
+        if (keys[38] || keys[87]) {     // up
+            if (this.moving === false) {    // the player scores a point when leaving the canvas, so we don't need to define a boundary condition here
+                this.y -= grid;   // frog will jump up 80 pixels
+                this.moving = true;
+            }
+        }
+        if (keys[40] || keys[83]) {     // down
+            if (this.moving === false && this.y < canvas.height - this.height * 2) {    // prevents player from leaving the canvas
+                this.y += grid;   // frog will jump up 80 pixels
+                this.moving = true;
+            }
+        }
+        if (keys[37] || keys[65]) {     // left
+            if (this.moving === false && this.x > this.width) {    // prevents player from leaving the canvas
+                this.x -= grid;   // frog will jump up 80 pixels
+                this.moving = true;
+            }
+        }
+        if (keys[39] || keys[68]) {     // right
+            if (this.moving === false && this.x < canvas.width - this.width * 2) {    // prevents player from leaving the canvas
+                this.x += grid;   // frog will jump up 80 pixels
+                this.moving = true;
+            }
+        }
     }
     draw() {
         ctx3.fillStyle = "green";
         ctx3.fillRect(this.x, this.y, this.width, this.height);
+    }
+    jump() {
+        console.log("jump");
     }
 }
 
--- assets/js/utilities.js
+++ assets/js/utilities.js
@@ -6,3 +6,18 @@ function animate() {
 }
 
 animate();
+
+// Event listeners
+
+window.addEventListener("keydown", function(e) {
+    keys = [];      // set keys to an empty array incase it holds any values
+    keys[e.keyCode] = true;
+    if ((keys[37] || keys[38] || keys[39] || keys[40]) || (keys[65] || keys[87] || keys[68] || keys[83])) {
+        frogger.jump();
+    }
+})
+
+window.addEventListener("keyup", function(e) {
+    delete keys[e.keyCode];
+    frogger.moving = false;
+})
