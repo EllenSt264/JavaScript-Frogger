cf7420af6b009695c7a2ae1f7a4669b3d4b0f5e4
EllenSt264 <stafford.ellen@protonmail.com>
2020-10-27 14:19:18 +0000

    Add feat: score board to count score, collisions  and game speed

--- assets/js/utilities.js
+++ assets/js/utilities.js
@@ -12,6 +12,7 @@ function animate() {
     frogger.draw();
     frogger.update();
     handleObstacles();
+    handleScoreBoard();
     ctx4.drawImage(grass, 0, 0, canvas.width, canvas.height);
     requestAnimationFrame(animate);
 }
@@ -35,8 +36,26 @@ window.addEventListener("keyup", function(e) {
 
 function scored() {
     score ++;
-    gamespeed =+ 0.05;  // to increase game difficulty
+    gamespeed += 0.05;  // to increase game difficulty
     // resets the frog once the player moves it past the top edge of the canvas
     frogger.x = canvas.width/2 - frogger.width/2;  
     frogger.y = canvas.height - frogger.height - 40;
+}
+
+// Score board
+
+function handleScoreBoard() {
+    // Score Board Text
+    ctx4.fillStyle = "black";
+    ctx4.strokeStyle = "black";
+    ctx4.font = "15px Verdana";
+    ctx4.strokeText("Score", 265, 15);
+    // Score Count
+    ctx4.font = "60px Verdana";
+    ctx4.fillText(score, 270, 65);
+    // Collisions Count
+    ctx4.font = "15px Verdana";
+    ctx4.strokeText("Collisions: " + collisionsCount, 35, 55);
+    // Game Speed Count
+    ctx4.strokeText("Game Speed: " + gamespeed.toFixed(1), 35, 75);     // toFixed(1) ensures we only see the gamespeed by 1 decimal point
 }
